4e03a080b8b81776671c4071d5f1becf4ffe914e
rahul22061999 <130198557+rahul22061999@users.noreply.github.com>
2024-04-14 00:23:31 -0400

    init

--- DSA concepts/binary.py
+++ DSA concepts/binary.py
@@ -20,13 +20,38 @@ class BinarySearchTree:
                 return False
             if new_node.value < temp.value:
                 if temp.left is None:
-                    print(f"Inserted {value} as left child of {temp.value}")
                     temp.left = new_node
                     return True 
                 temp = temp.left
             else:
                 if temp.right is None:
-                    print(f"Inserted {value} as right child of {temp.value}")
                     temp.right = new_node
                     return True 
                 temp = temp.right
+
+    def contains(self, value):
+        if self.root is None:
+            return False
+        temp = self.root 
+        while temp is not None:
+            if value < temp.value:
+                temp = temp.left 
+            elif value > temp.value:
+                temp = temp.right
+            else: 
+                return True 
+        return False
+
+
+my_tree = BinarySearchTree()
+my_tree.insert(2)
+my_tree.insert(1)
+my_tree.insert(3)
+my_tree.insert(9)
+my_tree.insert(7)
+my_tree.insert(87)
+my_tree.insert(96)
+
+
+print(my_tree.contains(96))
+print(my_tree.contains(89))
